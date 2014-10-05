14841fd76ff40cab1dc57fff2c7e8d8d13e9ef86
Kevin <kevinlin505@hotmail.com>
2014-10-04 20:21:18 -0400

    FirstTry
    homepage

--- js/carousel.js
+++ js/carousel.js
@@ -3,12 +3,13 @@
  */
 var carouselApp = angular.module('carouselApp', ['ui.bootstrap']);
 carouselApp.controller('homeCarousel',function($scope) {
-    $scope.myInterval = 10000;
-    var slides = $scope.slides = [{image: 'img/travel.jpg', name:'travel'},
-        {image: 'img/airport.jpg', name: 'airport'},
-        {image: 'img/balloons.jpg', name: 'balloons'},
-        {image: 'img/guides.jpg', name: 'guides'},
-        {image: 'img/planes.jpg', name: 'planes'},
-        {image: 'img/sites.jpg', name: 'sites'},
-        {image: 'img/memories.jpg', name: 'memories'}];
+    $scope.myInterval = 5000;
+    var slides = $scope.slides = [
+        {image: 'img/ace.jpg', name: 'ace'},
+        {image: 'img/tutor.jpg', name: 'tutor'},
+        {image: 'img/blackboard.jpg', name: 'blackboard'},
+        {image: 'img/homework.jpg', name: 'homework'},
+        {image: 'img/course.jpg', name:'course'},
+        {image: 'img/books.png', name: 'books'},
+        {image: 'img/kids.jpg', name: 'kids'}];
 });
--- js/carouselApp.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Kevin on 7/31/14.
- */
