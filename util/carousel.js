b57a2c3399ef5b41909d8b1554bef2e8a2e9c8f4
Kevin <kevinlin505@hotmail.com>
2015-01-20 20:25:18 -0500

    carousel

--- util/carousel.js
+++ util/carousel.js
@@ -6,8 +6,11 @@ var carouselApp = angular.module('carouselApp', ['ui.bootstrap'])
 carouselApp.controller('homeCarousel',function($scope) {
     $scope.myInterval = 5000;
     var slides = $scope.slides = [
-        {image: 'img/ace.jpg', name: 'ace'},
-        {image: 'img/tutor.jpg', name: 'tutor'},
-        {image: 'img/blackboard.jpg', name: 'blackboard'},
-        {image: 'img/kids.jpg', name: 'kids'}];
+        {image: '../style/img/slide1.jpg', name: 'Soho Vascular'},
+        {image: '../style/img/slide2.jpg', name: 'Soho Vascular'},
+        {image: '../style/img/slide3.jpg', name: 'Soho Vascular'},
+        {image: '../style/img/slide4.jpg', name: 'Soho Vascular'},
+        {image: '../style/img/slide5.jpg', name: 'Soho Vascular'},
+        {image: '../style/img/slide6.jpg', name: 'Soho Vascular'},
+        {image: '../style/img/slide7.jpg', name: 'Soho Vascular'},];
 });
--- util/main.js
+++ util/main.js
@@ -1,7 +1,21 @@
 /**
  * Created by Kevin on 10/8/2014.
  */
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ui.bootstrap']);
+
+
+app.controller('homeCarousel',function($scope) {
+    $scope.myInterval = 5000;
+    var slides = $scope.slides = [
+        {image: '../style/img/slide1.jpg', name: 'Soho Vascular'},
+        {image: '../style/img/slide2.jpg', name: 'Soho Vascular'},
+        {image: '../style/img/slide3.jpg', name: 'Soho Vascular'},
+        {image: '../style/img/slide4.jpg', name: 'Soho Vascular'},
+        {image: '../style/img/slide5.jpg', name: 'Soho Vascular'},
+        {image: '../style/img/slide6.jpg', name: 'Soho Vascular'},
+        {image: '../style/img/slide7.jpg', name: 'Soho Vascular'},];
+});
+
 app.directive("scroll", function($window){
     return function(scope, element, attrs){
         angular.element($window).bind("scroll", function(){
