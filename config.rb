a9b5436dbb3241caacd0704b550a3e515f952dd7
Kevin <kevinlin505@hotmail.com>
2015-01-21 00:00:12 -0500

    svs draft

---
+++ config.rb
@@ -0,0 +1,13 @@
+require 'singularitygs'
+
+
+project_type = :stand_alone
+http_path = "/"
+sass_dir = "style/sass"
+css_dir = "style/css"
+images_dir = "style/img"
+javascript_dir = "util"
+line_comments = false
+preferred_syntax = :scss
+output_style = :expanded
+relative_assests = true
--- util/main.js
+++ util/main.js
@@ -3,17 +3,27 @@
  */
 var app = angular.module('myApp', ['ui.bootstrap']);
 
+app.controller('imageLoader', function($scope){
+    $scope.missionImgs = ['style/img/mission1.png',
+                        'style/img/mission2.png',
+                        'style/img/mission3.png',
+                        'style/img/mission4.png'];
+});
+
+app.controller('footerCtrl', function($scope){
+    $scope.footerNavs = ['Home', 'About Us', 'Our Services', 'Physician Resources', 'Patient Resources', 'Why SOHO?', 'Contact Us'];
+});
 
 app.controller('homeCarousel',function($scope) {
     $scope.myInterval = 5000;
     var slides = $scope.slides = [
-        {image: '../style/img/slide1.jpg', name: 'Soho Vascular'},
-        {image: '../style/img/slide2.jpg', name: 'Soho Vascular'},
-        {image: '../style/img/slide3.jpg', name: 'Soho Vascular'},
-        {image: '../style/img/slide4.jpg', name: 'Soho Vascular'},
-        {image: '../style/img/slide5.jpg', name: 'Soho Vascular'},
-        {image: '../style/img/slide6.jpg', name: 'Soho Vascular'},
-        {image: '../style/img/slide7.jpg', name: 'Soho Vascular'},];
+        {image: '../Front-end/style/img/slide1.jpg', name: 'Soho Vascular'},
+        {image: '../Front-end/style/img/slide2.jpg', name: 'Soho Vascular'},
+        {image: '../Front-end/style/img/slide3.jpg', name: 'Soho Vascular'},
+        {image: '../Front-end/style/img/slide4.jpg', name: 'Soho Vascular'},
+        {image: '../Front-end/style/img/slide5.jpg', name: 'Soho Vascular'},
+        {image: '../Front-end/style/img/slide6.jpg', name: 'Soho Vascular'},
+        {image: '../Front-end/style/img/slide7.jpg', name: 'Soho Vascular'},];
 });
 
 app.directive("scroll", function($window){
