9e209d93eb72b77011714dccbc731a08b450c61b
Kevin <kevinlin505@hotmail.com>
2015-04-02 18:47:51 -0400

    Old work

--- util/carousel.js
+++ util/carousel.js
@@ -6,11 +6,8 @@ var carouselApp = angular.module('carouselApp', ['ui.bootstrap'])
 carouselApp.controller('homeCarousel',function($scope) {
     $scope.myInterval = 5000;
     var slides = $scope.slides = [
-        {image: '../style/img/slide1.jpg', name: 'Soho Vascular'},
-        {image: '../style/img/slide2.jpg', name: 'Soho Vascular'},
-        {image: '../style/img/slide3.jpg', name: 'Soho Vascular'},
-        {image: '../style/img/slide4.jpg', name: 'Soho Vascular'},
-        {image: '../style/img/slide5.jpg', name: 'Soho Vascular'},
-        {image: '../style/img/slide6.jpg', name: 'Soho Vascular'},
-        {image: '../style/img/slide7.jpg', name: 'Soho Vascular'},];
+        {image: 'img/ace.jpg', name: 'ace'},
+        {image: 'img/tutor.jpg', name: 'tutor'},
+        {image: 'img/blackboard.jpg', name: 'blackboard'},
+        {image: 'img/kids.jpg', name: 'kids'}];
 });
--- util/main.js
+++ util/main.js
@@ -1,31 +1,7 @@
 /**
  * Created by Kevin on 10/8/2014.
  */
-var app = angular.module('myApp', ['ui.bootstrap']);
-
-app.controller('imageLoader', function($scope){
-    $scope.missionImgs = ['style/img/mission1.png',
-                        'style/img/mission2.png',
-                        'style/img/mission3.png',
-                        'style/img/mission4.png'];
-});
-
-app.controller('footerCtrl', function($scope){
-    $scope.footerNavs = ['Home', 'About Us', 'Our Services', 'Physician Resources', 'Patient Resources', 'Why SOHO?', 'Contact Us'];
-});
-
-app.controller('homeCarousel',function($scope) {
-    $scope.myInterval = 5000;
-    var slides = $scope.slides = [
-        {image: '../Front-end/style/img/slide1.jpg', name: 'Soho Vascular'},
-        {image: '../Front-end/style/img/slide2.jpg', name: 'Soho Vascular'},
-        {image: '../Front-end/style/img/slide3.jpg', name: 'Soho Vascular'},
-        {image: '../Front-end/style/img/slide4.jpg', name: 'Soho Vascular'},
-        {image: '../Front-end/style/img/slide5.jpg', name: 'Soho Vascular'},
-        {image: '../Front-end/style/img/slide6.jpg', name: 'Soho Vascular'},
-        {image: '../Front-end/style/img/slide7.jpg', name: 'Soho Vascular'},];
-});
-
+var app = angular.module('myApp', []);
 app.directive("scroll", function($window){
     return function(scope, element, attrs){
         angular.element($window).bind("scroll", function(){
